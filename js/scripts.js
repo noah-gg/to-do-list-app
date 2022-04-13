9aa807732ab2bf312c71be27b0bbf7654539fddd
noah-gg <99127702+noah-gg@users.noreply.github.com>
2022-04-13 19:03:21 +0100

    Add strike-through, delete and reorder feature
    If applied, this commit will add the following features:
    - Strike-through on double-click with toggle
    - Delete the item from list with crossOutButton X
    - Re-order items with .sortable() on list

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,26 @@ function newItem() {
         $('#list').append(li);
     }
 
+    //  user cross out an item from list of item when item clicked
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+
+    li.on('dblclick', function crossOut() {
+        li.toggleClass('strike');
+    });
+
+    //  user delete an item from list of item when x button clicked
+    let crossOutButton = $('<deleteButton></deleteButton>');
+    crossOutButton.append(document.createTextNode(' X '));
+    li.append(crossOutButton);
+    crossOutButton.on('click', deleteListItem);
+
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+    
+    //  user change the order of items from list of item
+    $('#list').sortable();
 }
 
